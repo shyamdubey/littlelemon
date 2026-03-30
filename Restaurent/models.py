a7a783274fce82e713e5858ff9d52dad3a8d7a3a
Shyam Dubey <shyam0853@gmail.com>
2026-03-30 17:21:45 +0530

    another commit

--- Restaurent/models.py
+++ Restaurent/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    id:models.BigAutoField(primary_key=True)
+    name:models.CharField(max_length=255)
+    no_of_guests:models.IntegerField()
+    booking_date:models.DateField(auto_now=False)
+
+
+
+class Menu(models.Model):
+    id:models.BigAutoField(primary_key=True)
+    title:models.CharField(max_length=255)
+    price:models.FloatField()
+    inventory:models.IntegerField()
--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,9 +74,16 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'little_lemon',
+        'USER':'root',
+        'PASSWORD':'Pass@123',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command':'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
